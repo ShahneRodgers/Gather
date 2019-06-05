a66fb80137fc32171077ef1b7eea40b0aec356c5
Shahne Rodgers <shahnerodgers@gmail.com>
2019-06-05 20:02:12 +1200

    Adds an admin panel, fixes some raw text so it can be translated and adds details to the login page.

--- lib/gather/accounts/error_handler.ex
+++ lib/gather/accounts/error_handler.ex
@@ -3,11 +3,12 @@ defmodule Gather.Accounts.ErrorHandler do
   import Phoenix.Controller, only: [put_flash: 3, redirect: 2]
 
   require Logger
+  require GatherWeb.Gettext
 
   def auth_error(conn, {type, reason}, _opts) do
     Logger.info("Authentication error of type: #{type} with reason #{reason}")
 
-    put_flash(conn, :info, "You must log in")
+    put_flash(conn, :info, GatherWeb.Gettext.gettext("You must log in"))
     |> redirect(to: "/login")
     |> halt()
   end
